3e40fd318aceb7dd143a89f4df779bc44fdb1483
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-05 18:17:06 +0500

    created models for banks app

---
+++ fintrack/banks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Bank(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class BankBranch(models.Model):
+    branch_name = models.CharField(max_length=100)
+    branch_code = models.CharField(max_length=100, unique=True)
+
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.branch_name} ({self.branch_code})"
