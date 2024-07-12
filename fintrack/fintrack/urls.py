610f28670a54d4399f313c942bb5e9ab6ca7ddcb
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-12 18:32:10 +0500

    .

--- fintrack/fintrack/urls.py
+++ fintrack/fintrack/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('transactions.urls')),
+    path('api/transactions', include('transactions.urls')),
 ]
