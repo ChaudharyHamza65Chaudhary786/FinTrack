5d6d484d27c8f164c0a46cd32fdd91d2c3c2ed3c
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-12 20:36:35 +0500

    eof

--- fintrack/users/helper.py
+++ fintrack/users/helper.py
@@ -8,6 +8,6 @@ class UserHelper:
         user = User.objects.get(username=user_name)
         refresh = RefreshToken.for_user(user)
         return {
-            'refresh':str(refresh),
-            'access':str(refresh.access_token)
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
         }
