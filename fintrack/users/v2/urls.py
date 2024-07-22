fcdb87b350fd6ca51ae06670b43b3e7158124d6e
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-22 16:52:34 +0500

    removes unecessary api's

--- fintrack/users/v2/urls.py
+++ fintrack/users/v2/urls.py
@@ -6,6 +6,4 @@ from . views import UserRegistrationView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('token/', views.TokenObtainPairView.as_view(), name='token_pair'),
-    path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh')
 ]
