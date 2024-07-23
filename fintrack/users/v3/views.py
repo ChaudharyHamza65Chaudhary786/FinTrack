9a30cb332935c2d9bb5a722cd36c2187c78b7762
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-23 16:02:03 +0500

    uses viewsets

---
+++ fintrack/users/v3/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from users.serializer import UserSerializer
+
+
+class RegistrationViewSet(viewsets.ViewSet):
+
+    permission_classes = []
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data)
