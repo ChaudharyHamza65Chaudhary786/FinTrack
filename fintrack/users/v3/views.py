b06decac94e407c6c2a11fc8015a1b94b1d94f8e
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-24 15:25:43 +0500

    indentation

--- fintrack/users/v3/views.py
+++ fintrack/users/v3/views.py
@@ -7,8 +7,10 @@ from users.serializer import UserSerializer
 class RegistrationViewSet(viewsets.ViewSet):
 
     permission_classes = []
+
     def create(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        
         return Response(serializer.validated_data)
