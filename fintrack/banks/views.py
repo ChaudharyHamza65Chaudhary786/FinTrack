2778d2034de30dd6e297fed3a75366e04ab1f3cd
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-26 14:05:11 +0500

    feneric views for banks

--- fintrack/banks/views.py
+++ fintrack/banks/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from django.http import HttpResponseForbidden
 
-# Create your views here.
+from .models import Bank, Branch
+from .serializer import BankSerializer, BranchSerializer
+
+
+class BankViewApi(ListCreateAPIView):
+    serializer_class = BankSerializer
+    queryset = Bank.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return HttpResponseForbidden("Permssion Denied")
+
+        return super().list(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return HttpResponseForbidden("Permssion Denied")
+        
+        return super().create(request, *args, **kwargs)
+    
+
+class BrachAPIView(ListCreateAPIView):
+    serialzer_class = BranchSerializer
+
+    def get_queryset(self):
+        return Branch.objects.filter(
+            bank__name=self.kwargs['bank']
+        )
--- fintrack/users/serializer.py
+++ fintrack/users/serializer.py
@@ -6,13 +6,14 @@ from . models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'address')
+        fields = ('username', 'email', 'password', 'address', 'is_staff')
 
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
             email=validated_data['email'],
-            address=validated_data['address']
+            address=validated_data['address'],
+            is_staff=validated_data['is_staff']
         )
         user.set_password(validated_data['password'])
         user.save()
