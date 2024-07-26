9e383d278c56ac690cbb7400a79eebf65c394428
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-26 16:06:42 +0500

    implements generic views

---
+++ fintrack/banks/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Bank, Branch
+
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = '__all__'
+
+
+class BankSerializer(serializers.ModelSerializer):
+    branches = BranchSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Bank
+        fields = ['name', 'branches']
---
+++ fintrack/banks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import BankAPIView, BranchAPIView, BankDetailAPIView
+urlpatterns = [
+    path('', BankAPIView.as_view(), name='banks'),
+    path('create-branch/', BranchAPIView.as_view(), name='create_branch'),
+    path('<int:pk>/', BankDetailAPIView.as_view(), name='bank_details')
+]
