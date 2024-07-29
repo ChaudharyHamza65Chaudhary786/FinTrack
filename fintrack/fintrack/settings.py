f26fc3b3e4adbe3b75090203a82a228ab5d970f0
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-29 16:53:57 +0500

    eol

--- fintrack/fintrack/settings.py
+++ fintrack/fintrack/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 STATIC_URL = 'static/'
 
